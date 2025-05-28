8aa297c33e56b399152b2598dd95c93521460c5a
nahid <md.nahidhossen1911@gmail.com>
2025-05-28 20:35:11 +0600

    Linear searching

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'dart:core';
+import 'searching.dart';
+
+void main() async {
+  final stopwatch = Stopwatch()..start();
+
+  print("Nahid");
+
+  // Linear searching
+  print(await Searching.linearSearch([60, 1, 88, 10, 11, 100], 100));
+
+  stopwatch.stop();
+  print('Operation completed in: ${stopwatch.elapsedMilliseconds} ms');
+}
+
+
---
+++ lib/searching.dart
@@ -0,0 +1,16 @@
+class Searching{
+
+  static Future<int> linearSearch(List list, item) async {
+    late int ret;
+    for (int i = 0; i < list.length; i++) {
+      if (list[i] == item) {
+        return i;
+      }
+    }
+    ret = -1;
+    return ret;
+  }
+
+
+
+}
